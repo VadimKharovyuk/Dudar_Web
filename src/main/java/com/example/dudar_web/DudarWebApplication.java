6113c2ef84e3055a858ed70947cb54c8455f4ef5
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-02-17 02:30:41 +0530

    Initial commit

---
+++ src/main/java/com/example/dudar_web/DudarWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.dudar_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DudarWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DudarWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dudar_web/DudarWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dudar_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DudarWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
